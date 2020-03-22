0a67d5f0788db6b2991bb835c82b7451dd17d280
MulesKC <rmmcp3@gmail.com>
2020-03-22 14:00:04 -0500

    finished SQL task 1

--- queries.sql
+++ queries.sql
@@ -1,7 +1,10 @@
 ## Part 1: Test it with SQL
-    DESCRIBE job
+    SELECT COLUMN_NAME, DATA_TYPE
+    FROM   INFORMATION_SCHEMA.COLUMNS
+    WHERE  TABLE_NAME = 'job' AND TABLE_SCHEMA = 'techjobs';
 
 ## Part 2: Test it with SQL
+
 
 ## Part 3: Test it with SQL
 
